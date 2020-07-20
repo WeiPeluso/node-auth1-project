445cab1387267e77c54a8360ecdad6bb170e177d
Wei Peluso <weipeluso@MacBook-Pro.local>
2020-07-20 15:12:24 -0400

    finished mvp and stretch

---
+++ api/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const helmet = require("helmet");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
+
+const usersRouter = require("../users/users-router.js");
+const authRouter = require("../auth/auth-router.js");
+const dbConnection = require("../database/connection.js");
+const authenticate = require("../auth/authenticate-middleware.js");
+
+const server = express();
+
+const sessionConfiguration = {
+  name: "user", // default value is sid
+  secret: process.env.SESSION_SECRET || "keep it secret, keep it safe!", // key for encryption
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: process.env.USE_SECURE_COOKIES || false, // send the cookie only over https (secure connections)
+    httpOnly: true, // prevent JS code on client from accessing this cookie
+  },
+  resave: false,
+  saveUninitialized: true, // read docs, it's related to GDPR compliance
+  store: new KnexSessionStore({
+    knex: dbConnection,
+    tablename: "sessions",
+    sidfieldname: "user",
+    createtable: true,
+    clearInterval: 1000 * 60 * 30, // time to check and remove expired sessions from database
+  }),
+};
+
+server.use(session(sessionConfiguration)); // enables session support
+server.use(helmet());
+server.use(express.json());
+
+server.use("/api/users", authenticate, usersRouter);
+server.use("/api/auth", authRouter);
+
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
+});
+
+module.exports = server;
